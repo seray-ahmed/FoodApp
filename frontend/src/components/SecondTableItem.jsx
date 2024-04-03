4282a5b78f1ec926353cda21178908cf411786b2
seray-ahmed <seray.ahmed1@gmail.com>
2024-04-04 00:17:19 +0300

    style everything and finalyze the project

--- frontend/src/components/SecondTableItem.jsx
+++ frontend/src/components/SecondTableItem.jsx
@@ -1,22 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const SecondTableItem = ({ data, setSelectedFoods }) => {
-    const deleteSelectedFood = (selected) => {
-        const foodToRemove = selected.id;
-        setSelectedFoods(prev => prev.filter(item => item.id !== foodToRemove))
-    }
-    return (
-        <>
-            <tr>
-                <td>{data.name}</td>
-                <td>{data.calories}</td>
-                <td>{data.proteins}</td>
-                <td>{data.carbs}</td>
-                <td>{data.fats}</td>
-                <td><button onClick={() => deleteSelectedFood(data)}>Delete</button></td>
-            </tr>
-        </>
-    )
-}
+  const deleteSelectedFood = (selected) => {
+    const foodToRemove = selected.id;
+    setSelectedFoods((prev) => prev.filter((item) => item.id !== foodToRemove));
+  };
+  return (
+    <>
+      <tr>
+        <td>{data.name}</td>
+        <td>{data.calories}</td>
+        <td>{data.proteins}</td>
+        <td>{data.carbs}</td>
+        <td>{data.fats}</td>
+        <td>
+          <button
+            onClick={() => deleteSelectedFood(data)}
+            class="btn btn-delete"
+          >
+            <span class="mdi mdi-delete mdi-24px"></span>
+            <span class="mdi mdi-delete-empty mdi-24px"></span>
+            <span>Delete</span>
+          </button>
+        </td>
+      </tr>
+    </>
+  );
+};
 
-export default SecondTableItem
+export default SecondTableItem;
